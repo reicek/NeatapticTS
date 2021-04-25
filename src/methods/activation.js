ec6f26a3f92cbf778dc3fef9391e60f96706a134
Cesar Anton <reicek@gmail.com>
2021-04-25 15:51:28 -0400

    refactor methods/activation

--- src/methods/activation.js
+++ src/methods/activation.js
@@ -1,79 +1,81 @@
-/*******************************************************************************
-                                  ACTIVATION FUNCTIONS
-*******************************************************************************/
+/**
+ * Activation function
+ * @see {@link https://en.wikipedia.org/wiki/Activation_function}
+ * @see {@link https://stats.stackexchange.com/questions/115258/comprehensive-list-of-activation-functions-in-neural-networks-with-pros-cons}
+ */
+export default class Activation {
+  static logistic(x, derivate) {
+    const fx = 1 / (1 + Math.exp(-x));
 
-// https://en.wikipedia.org/wiki/Activation_function
-// https://stats.stackexchange.com/questions/115258/comprehensive-list-of-activation-functions-in-neural-networks-with-pros-cons
-var activation = {
-  LOGISTIC: function (x, derivate) {
-    var fx = 1 / (1 + Math.exp(-x));
-    if (!derivate) return fx;
-    return fx * (1 - fx);
-  },
-  TANH: function (x, derivate) {
-    if (derivate) return 1 - Math.pow(Math.tanh(x), 2);
-    return Math.tanh(x);
-  },
-  IDENTITY: function (x, derivate) {
+    return !derivate ? fx : fx * (1 - fx);
+  }
+
+  static tanh(x, derivate) {
+    return derivate ? 1 - Math.pow(Math.tanh(x), 2) : Math.tanh(x);
+  }
+
+  static identity(x, derivate) {
     return derivate ? 1 : x;
-  },
-  STEP: function (x, derivate) {
-    return derivate ? 0 : x > 0 ? 1 : 0;
-  },
-  RELU: function (x, derivate) {
-    if (derivate) return x > 0 ? 1 : 0;
-    return x > 0 ? x : 0;
-  },
-  SOFTSIGN: function (x, derivate) {
-    var d = 1 + Math.abs(x);
-    if (derivate) return x / Math.pow(d, 2);
-    return x / d;
-  },
-  SINUSOID: function (x, derivate) {
-    if (derivate) return Math.cos(x);
-    return Math.sin(x);
-  },
-  GAUSSIAN: function (x, derivate) {
-    var d = Math.exp(-Math.pow(x, 2));
-    if (derivate) return -2 * x * d;
-    return d;
-  },
-  BENT_IDENTITY: function (x, derivate) {
-    var d = Math.sqrt(Math.pow(x, 2) + 1);
-    if (derivate) return x / (2 * d) + 1;
-    return (d - 1) / 2 + x;
-  },
-  BIPOLAR: function (x, derivate) {
-    return derivate ? 0 : x > 0 ? 1 : -1;
-  },
-  BIPOLAR_SIGMOID: function (x, derivate) {
-    var d = 2 / (1 + Math.exp(-x)) - 1;
-    if (derivate) return (1 / 2) * (1 + d) * (1 - d);
-    return d;
-  },
-  HARD_TANH: function (x, derivate) {
-    if (derivate) return x > -1 && x < 1 ? 1 : 0;
-    return Math.max(-1, Math.min(1, x));
-  },
-  ABSOLUTE: function (x, derivate) {
-    if (derivate) return x < 0 ? -1 : 1;
-    return Math.abs(x);
-  },
-  INVERSE: function (x, derivate) {
-    if (derivate) return -1;
-    return 1 - x;
-  },
-  // https://arxiv.org/pdf/1706.02515.pdf
-  SELU: function (x, derivate) {
-    var alpha = 1.6732632423543772848170429916717;
-    var scale = 1.0507009873554804934193349852946;
-    var fx = x > 0 ? x : alpha * Math.exp(x) - alpha;
-    if (derivate) {
-      return x > 0 ? scale : (fx + alpha) * scale;
-    }
-    return fx * scale;
-  },
-};
+  }
 
-/* Export */
-module.exports = activation;
+  static step(x, derivate) {
+    return derivate ? 0 : x > 0 ? 1 : 0;
+  }
+
+  static relu(x, derivate) {
+    return derivate ? (x > 0 ? 1 : 0) : x > 0 ? x : 0;
+  }
+
+  static softsign(x, derivate) {
+    const d = 1 + Math.abs(x);
+
+    return derivate ? x / Math.pow(d, 2) : x / d;
+  }
+
+  static sinusoid(x, derivate) {
+    return derivate ? Math.cos(x) : Math.sin(x);
+  }
+
+  static gaussian(x, derivate) {
+    const d = Math.exp(-Math.pow(x, 2));
+
+    return derivate ? -2 * x * d : d;
+  }
+
+  static bentIdentity(x, derivate) {
+    const d = Math.sqrt(Math.pow(x, 2) + 1);
+
+    return derivate ? x / (2 * d) + 1 : (d - 1) / 2 + x;
+  }
+
+  static bipolar(x, derivate) {
+    return derivate ? 0 : x > 0 ? 1 : -1;
+  }
+
+  static bipolarSigmoid(x, derivate) {
+    const d = 2 / (1 + Math.exp(-x)) - 1;
+
+    return derivate ? (1 / 2) * (1 + d) * (1 - d) : d;
+  }
+
+  static hardTanh(x, derivate) {
+    return derivate ? (x > -1 && x < 1 ? 1 : 0) : Math.max(-1, Math.min(1, x));
+  }
+
+  static absolute(x, derivate) {
+    return derivate ? (x < 0 ? -1 : 1) : Math.abs(x);
+  }
+
+  static inverse(x, derivate) {
+    return derivate ? -1 : 1 - x;
+  }
+
+  /** @see {@link https://arxiv.org/pdf/1706.02515.pdf} */
+  static selu(x, derivate) {
+    const alpha = 1.6732632423543772848170429916717;
+    const scale = 1.0507009873554804934193349852946;
+    const fx = x > 0 ? x : alpha * Math.exp(x) - alpha;
+
+    return derivate ? (x > 0 ? scale : (fx + alpha) * scale) : fx * scale;
+  }
+}
